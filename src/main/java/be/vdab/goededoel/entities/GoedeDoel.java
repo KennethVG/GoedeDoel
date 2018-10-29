e43d1c44d421520a2334e2df6966668559a0dc98
kenneth.vangijsel <kenneth.vangijsel@vdab.be>
2018-10-29 15:37:43 +0100

    Fixed test

--- src/main/java/be/vdab/goededoel/entities/GoedeDoel.java
+++ src/main/java/be/vdab/goededoel/entities/GoedeDoel.java
@@ -3,15 +3,18 @@ package be.vdab.goededoel.entities;
 import java.math.BigDecimal;
 
 public class GoedeDoel {
-	private final String naam;
-	private BigDecimal opgebracht;
-	public GoedeDoel(String naam) {
-		this.naam = naam;
-	}
-	public String getNaam() {
-		return naam;
-	}
-	public BigDecimal getOpgebracht() {
-		return opgebracht;
-	}
+    private final String naam;
+    private BigDecimal opgebracht = BigDecimal.ZERO;
+
+    public GoedeDoel(String naam) {
+        this.naam = naam;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public BigDecimal getOpgebracht() {
+        return opgebracht;
+    }
 }
