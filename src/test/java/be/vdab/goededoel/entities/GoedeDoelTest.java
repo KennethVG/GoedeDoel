e9b4c1b9756034c93591e7052b6f4f9c7accf970
kenneth.vangijsel <kenneth.vangijsel@vdab.be>
2018-10-30 11:48:51 +0100

    Javadoc

--- src/test/java/be/vdab/goededoel/entities/GoedeDoelTest.java
+++ src/test/java/be/vdab/goededoel/entities/GoedeDoelTest.java
@@ -1,13 +1,12 @@
 package be.vdab.goededoel.entities;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Before;
 import org.junit.Test;
 
 import java.math.BigDecimal;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class GoedeDoelTest {
     private static final String NAAM = "CliniClowns";
